d575130bda5ef7633c48e9bec2f993e4fc9fc97e
seven1106 <that9011@gmail.com>
2023-10-07 10:46:33 +0700

    init config

---
+++ app/src/main/java/com/example/ctut_student/activities/DashboardActivity.java
@@ -0,0 +1,16 @@
+package com.example.ctut_student.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ctut_student.R;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
